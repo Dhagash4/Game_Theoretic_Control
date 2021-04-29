e905cfd7f97a48239b2b6297418d80c284704ff8
Saurabh Gupta <saurabh@pop-os.localdomain>
2021-04-29 11:43:08 +0200

    Stanley Control on Carla v1.0

--- carla_scripts/stanley_control.py
+++ carla_scripts/stanley_control.py
@@ -4,8 +4,6 @@ import sys
 import random
 import time
 import numpy as np
-
-from stanley_control import *
 from util import *
 from datetime import datetime
 
@@ -33,8 +31,8 @@ class CarEnv():
         self.world = self.client.get_world()
 
         # Get Map of the world
-        self.map = world.get_map()
-
+        self.map = self.world.get_map()
+        
         # Get all the blueprints available in the world
         blueprint_library =  self.world.get_blueprint_library()
 
@@ -42,7 +40,7 @@ class CarEnv():
         self.car_model = blueprint_library.find('vehicle.tesla.model3')
 
         # Spawn vehicle at first waypoint
-        self.spawn_point = carla.transform(carla.location(waypoints[0, 0], waypoints[0, 1], 2), carla.rotation(0, waypoints[0, 2], 0))
+        self.spawn_point = carla.Transform(carla.Location(waypoints[0, 0], waypoints[0, 1], 2), carla.Rotation(0, waypoints[0, 2], 0))
         self.vehicle =  self.world.spawn_actor(self.car_model, self.spawn_point)
 
         # Get max steering angle of car's front wheels
@@ -57,8 +55,8 @@ class CarEnv():
         self.prev_t = 0.0
         self.prev_v_error = 0.0
         self.prev_throttle = 0.0
+        self.prev_brake = 0.0
         self.int_error_v = 0.0
-
     
     def destroy(self):
         ''' Destroy all actors in the world
@@ -67,7 +65,7 @@ class CarEnv():
             actor.destroy()
 
 
-    def longitudinal_controller(v: float, v_des: float, prev_err: float, cumulative_error: float, tuning_param: list, dt: float):
+    def longitudinal_controller(self,v: float, v_des: float, prev_err: float, cumulative_error: float, tuning_param: list, dt: float):
         """
         Compute control signal (acceleratio/deceleration) for linear velocity control
         Args:
@@ -96,74 +94,120 @@ class CarEnv():
         return acc, curr_err, cumulative_error
 
 
-    def calculate_target_index(x_r, y_r, xs_des: list, ys_des: list()):
+    def calculate_target_index(self, x_r, y_r, xs_des: list, ys_des: list(), lookahead = 2):
         # Search nearest waypoint index
-        dx = [xr - x for x in xs_des]
-        dy = [yr - y for y in ys_des]
+        dx = [x_r - x for x in xs_des]
+        dy = [y_r - y for y in ys_des]
         dist = np.sqrt(np.power(dx, 2) + np.power(dy, 2))
-        min_ind = np.argmin(dist)
-        min_dist = np.min(dist)
+        min_ind = (np.argmin(dist) + lookahead) % len(xs_des)
+        min_dist = dist[min_ind]
 
         return min_ind, min_dist
 
+    # def logging_data(self,idx,d,psi_h,yaw_diff,yaw_v,yaw_des,psi_c,_throttle,_brake,ke,x_des,y_des,x_v,y_v,_steer):
+    #         with open("datalog_run_%f.txt"%(ke),"a+") as f:
+    #             # f.write("ID:%d, Distance:%f, Heading_Error:%f, Yaw_Difference:%f,Yaw_Vehicle:%f, Yaw_des:%f, Cross_Track_Error:%f, Steer:%f, Throttle:%f, Brake:%f,Vehicle_x:%f,Vehicle_y:%f,x_des:%f,y_des:%f\n"%(idx,d,psi_h,yaw_diff,yaw,yaw_des,psi_c,_steer,_throttle,_brake))
+
+
+    #             f.write("ID:%d, Distance:%f, Vehicle_x:%f, Vehicle_y:%f, Yaw_Vehicle:%f, x_des:%f, y_des:%f, Yaw_des:%f, Heading_Error:%f, Yaw_Difference:%f,  Cross_Track_Error:%f, Steer:%f, Throttle:%f, Brake:%f\n"%(idx, d, x_v, y_v, yaw_v, x_des, y_des, yaw_des, psi_h, yaw_diff, psi_c, _steer, _throttle, _brake))
+
+    #             f.close()
 
     def stanley_control(self, waypoints):
-        # Saving the state before control
-        acc_before = self.vehicle.get_acceleration()
-        ang_vel_before = self.vehicle.get_angular_velocity()
-        vel_before = self.vehicle.get_velocity()
-        loc_before = self.vehicle.get_transform()
-
         #Applying Control to the Car
-        
+
         # Desired velocity [m/s]
-        v_des = 1.0
+        v_des = 14.0
 
         # Desired trajectory
         x_des = waypoints[:,0]
         y_des = waypoints[:,1]
-        yaw_des = [wrapToPi(np.radians(i)) for i in waypoints[:, 2]]
+        yaw_des = [wrapToPi(np.radians(i)) for i in waypoints[:, 2]] # [radians] 
         
         # Initial velocity error
-        err_v = v_des - self.vehicle.get_velocity().x
+        err_v = v_des - np.sqrt((self.vehicle.get_velocity().x) ** 2 + (self.vehicle.get_velocity().y) ** 2)
+
         err_total_v = 0
 
         # Crosstrack error control gains
         # Smooth recovery
-        ke = 0.8
+        self.ke = 0.1
         # Low velocity gain
-        kv = 0.5
+        self.kv = 10
 
         # PID tuning parameters
-        kp_lon, kd_lon, ki_lon = 1.0, 0.0, 0.0
+        kp_lon, kd_lon, ki_lon = 0.5, 0.1, 0.0
 
+        self.track_idx = []
+        self.track_d = []
+        self.track_y_v = []
+        self.track_x_v = []
+        self.track_yaw_v = []
+        self.track_v = []
+                
+        self.track_x_des = []        
+        self.track_y_des = []
+        self.track_yaw_des = []
+
+        self.track_steer = []
+        self.track_throttle = []
+        self.track_brake = []
+        self.track_head_err = []
+        self.track_cross_err = []
+
+        i = 0
         while 1:
+            i += 1
             x = self.vehicle.get_transform().location.x
             y = self.vehicle.get_transform().location.y
             yaw = self.vehicle.get_transform().rotation.yaw     # [degrees]
-            v = self.vehicle.get_velocity().x
-            curr_t = carla.timestamp.elapsed_seconds() # [seconds]
 
+            self.track_x_v.append(x)
+            self.track_y_v.append(y)
+            self.track_yaw_v.append(yaw)
+            
+            v = np.sqrt((self.vehicle.get_velocity().x) ** 2 + (self.vehicle.get_velocity().y) ** 2)
+            self.track_v.append(v)
+
+            self.snapshot = self.world.wait_for_tick()
+            curr_t = self.snapshot.timestamp.elapsed_seconds # [seconds]
+            # print(curr_t,self.prev_t)
             # Velocity control
             dt = curr_t - self.prev_t
-            acc, self.prev_v_error, self.int_error_v = longitudinal_controller(
+            # dt = 1.0            
+            acc, self.prev_v_error, self.int_error_v = self.longitudinal_controller(
                 v, v_des, self.prev_v_error, self.int_error_v, [kp_lon, ki_lon, kd_lon], dt)
 
             # Find nearest waypoint
-            idx, d = calculate_target_index(x, y, x_des, y_des)
+            idx, d = self.calculate_target_index(x, y, x_des, y_des, 3)
 
+            self.track_idx.append(idx)
+            self.track_d.append(d)
+            self.track_x_des.append(x_des[idx])
+            self.track_y_des.append(y_des[idx])
+            self.track_yaw_des.append(np.degrees(yaw_des[idx]))
+
+            self.world.debug.draw_string(carla.Location(waypoints[idx, 0], waypoints[idx, 1], 2), '.', draw_shadow=False,
+                                   color=carla.Color(r=255, g=0, b=0), life_time=5,
+                                   persistent_lines=True)
             # Heading error [radians]
-            psi_h = wrapToPi(np.radians(yaw_des[idx] - yaw))
+            psi_h = wrapToPi(yaw_des[idx] - np.radians(yaw))
+
+            self.track_head_err.append(psi_h)
 
             # Crosstrack yaw difference to path yaw [radians]
-            yaw_diff = wrapToPi(np.radians(yaw_des[idx]) - np.arctan2(y - y_des[idx], x - x_des[idx]))
-
+            yaw_diff = wrapToPi(yaw_des[idx] - np.arctan2(y - y_des[idx], x - x_des[idx]))
+        
             # Crosstrack error in yaw [radians]
-            psi_c = np.arctan2(ke * np.sign(yaw_diff) * d, kv + v)
+            psi_c = np.arctan2(self.ke * np.sign(yaw_diff) * d, self.kv + v)
+
+            self.track_cross_err.append(psi_c)
 
             # Steering angle control
             _steer = np.degrees(wrapToPi(psi_h + psi_c))  # uncontrained in degrees
-            _steer = max(min(_steer, self.max_steer_angle), -self.max_steer_angle) # constrained to [-1, 1]
+            _steer = max(min(_steer, self.max_steer_angle), -self.max_steer_angle)
+            _steer = (_steer)/self.max_steer_angle # constrained to [-1, 1]
+            self.track_steer.append(_steer)
 
             # Split velocity control into throttle and brake and constrain them to [0, 1]
             if acc >= 0:
@@ -173,6 +217,21 @@ class CarEnv():
                 _throttle = 0
                 _brake = np.tanh(abs(acc))
             
+            if(_brake == 0):
+                if (_throttle - self.prev_throttle) > 0.1:
+                    _throttle = self.prev_throttle + 0.1
+            
+            if(_throttle == 0):
+                if (_brake - self.prev_brake) > 0.1:
+                    _brake = self.prev_brake + 0.1
+            
+            self.prev_throttle = _throttle
+            self.prev_brake = _brake
+            self.track_throttle.append(_throttle)
+            self.track_brake.append(_brake)
+
+            # self.logging_data(idx,d,psi_h,yaw_diff,psi_c,_throttle,_brake,yaw,yaw_des[idx],ke,x_des[idx],y_des[idx],x,y,_steer)
+            
             # Apply control
             self.vehicle.apply_control(carla.VehicleControl(
                 throttle = _throttle, steer = _steer, reverse = False, brake = _brake))
@@ -181,7 +240,7 @@ class CarEnv():
             
         #After Control Information
 
-
+        
 def read_waypoints_file(path):
     ''' Read waypoints list
     '''
@@ -199,6 +258,15 @@ def main():
         env.stanley_control(waypoints)
     
     finally:
+        data_log_vehicle = np.vstack((env.track_x_v, env.track_y_v, env.track_yaw_v, env.track_v))
+        data_log_waypoint = np.vstack((env.track_idx, env.track_d, env.track_x_des, env.track_y_des, env.track_yaw_des))
+        data_log_error = np.vstack((env.track_head_err, env.track_cross_err))
+        data_log_control = np.vstack((env.track_steer, env.track_throttle, env.track_brake))
+        
+        np.savetxt("datalog_veh_%f_%f.txt"%(env.ke, env.kv), data_log_vehicle.T)
+        np.savetxt("datalog_way_%f_%f.txt"%(env.ke, env.kv), data_log_waypoint.T)
+        np.savetxt("datalog_err_%f_%f.txt"%(env.ke, env.kv), data_log_error.T)
+        np.savetxt("datalog_con_%f_%f.txt"%(env.ke, env.kv), data_log_control.T)
         env.destroy()
 
 if __name__=="__main__":
